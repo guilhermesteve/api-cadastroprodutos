e7ce93bda3a16fa6ca3d5a31a89e58791d562eb9
guilherme soares <guilhermesoares134@gmail.com>
2022-05-21 19:37:54 -0300

    refactor: add express errors to errors control

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import * as errorhandler from "express-async-errors";
 
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specification.routes";
@@ -9,4 +10,11 @@ app.use(express.json());
 app.use("/categories", categoriesRoutes);
 
 app.use("/specification", specificationRoutes);
+
+app.use((err, req, res, next) => {
+  res.json({ error: err.message });
+
+  next(err);
+});
+
 app.listen(3333);
