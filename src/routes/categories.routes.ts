1b49b173a3d1a11f8c66932b62481eed6e2cbc75
guilherme soares <guilhermesoares134@gmail.com>
2022-05-26 21:28:15 -0300

    refacotor: add upload of the category in file csv

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/CreateCategory";
+import { importCategoryController } from "../modules/cars/useCases/ImportCategory";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
+const upload = multer({
+  dest: "./tmp",
+});
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (req, res) => {
@@ -11,6 +16,10 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("/", (req, res) => {
   return listCategoriesController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
